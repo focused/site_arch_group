471347fc544e64fc62d41cb702ab3b780075e77e
Dmitry Novikov <glowinglens@gmail.com>
2013-10-08 18:56:23 +0400

    make projects in frontend visible

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,10 +20,17 @@ class ProjectsController < SiteController
     else
       @category.projects
     end
+
+    if @projects
+      @projects.each_with_index do |pr, n|
+        @projects[n] = pr.parent if pr.parent
+      end
+    end
   end
 
   def show
     @project = Project.find params[:id]
+    @project = @project.parent if @project.parent
     meta_tags title: [@project.title]
   end
 end
