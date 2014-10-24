18aa56cacaa44c060f92c02e1089889a87bfe321
Dmitry Novikov <glowinglens@gmail.com>
2014-10-24 17:45:32 +0400

    improve share fb button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def unwrap_tag(text, tag = p)
     text.sub(/\A[<]p[>]/, '').sub(/[<]p[>]\z/, '')
   end
+
+  def lang_iso_fb(code = r18n.locale.code)
+    {
+      ru: "ru_RU",
+      en: "en_US"
+    }[code.to_sym]
+  end
 end
